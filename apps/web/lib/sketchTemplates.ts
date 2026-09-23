18702156fb77c87cfd16b9bd3a66aba5a132b354
kabelohlaps-cmyk <kabelohlaps@gmail.com>
2026-09-23 21:35:47 +0200

    Update sketchTemplates.ts

--- apps/web/lib/sketchTemplates.ts
+++ apps/web/lib/sketchTemplates.ts
@@ -1,1 +1,39 @@
+export interface SketchTemplate {
+  id: string;
+  label: string;
+  description: string;
+  url: string;
+}
 
+export const SKETCH_TEMPLATES: SketchTemplate[] = [
+  {
+    id: "none",
+    label: "No Template",
+    description: "Start with a blank canvas.",
+    url: "",
+  },
+  {
+    id: "humanoid-gesture",
+    label: "Humanoid Gesture",
+    description: "Basic standing pose skeleton for characters or mecha.",
+    url: "/sketch-templates/humanoid-gesture.svg",
+  },
+  {
+    id: "perspective-grid",
+    label: "Perspective Grid",
+    description: "Single vanishing-point grid for environments and props.",
+    url: "/sketch-templates/perspective-grid.svg",
+  },
+  {
+    id: "head-proportions",
+    label: "Head Proportions",
+    description: "Cranium guide with standard facial proportion lines.",
+    url: "/sketch-templates/head-proportions.svg",
+  },
+  {
+    id: "vehicle-box",
+    label: "Vehicle Box",
+    description: "3D construction box for vehicles and hard-surface tech.",
+    url: "/sketch-templates/vehicle-box.svg",
+  },
+];
